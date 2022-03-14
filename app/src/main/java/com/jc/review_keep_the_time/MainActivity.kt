bb66cdd617a69cbe393808b293527ea734883802
JaeCheon KO <jaecheon@JaeCheons-MacBook-Pro.local>
2022-03-14 13:06:28 +0900

    약속 지킴이 리뷰 프로젝트 생성

---
+++ app/src/main/java/com/jc/review_keep_the_time/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jc.review_keep_the_time
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
